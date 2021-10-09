f6a21090de59b080dd05ecabee04fa261df42424
Leo Bowen Wang <59091252+leobowenwang@users.noreply.github.com>
2021-10-09 22:30:30 +0200

    first commit

---
+++ api.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const { IP, PORT } = require("./address.json");
+
+app.use(express.json());
+
+app.listen(PORT, IP, () => console.log("API running on " + IP + ":" + PORT));
+
+app.get("/api/sensor", (req, res) => {
+  res.status(200).send({
+    msg: 12345
+  });
+});
+
+app.post("/api/sensor/data", (req, res) => {
+  const { key, temp, hum, status } = req.body;
+
+  if (!key) {
+    res.status(418).send({ msg: "418 I'm a teapot" });
+  }
+
+  if (status == 404) {
+    res.status(404).send({ msg: "404 Failed to read sensor" });
+  }
+  if (status == 400) {
+    res.status(400).send({ msg: "400 Invalid sensor readings" });
+  }
+  if ((key, temp, hum)) {
+    res.status(200).send({
+      key: key,
+      temp: temp,
+      hum: hum,
+    });
+    /*try {
+    contract(key, temp);
+  } catch(error) {
+    console.log(error);
+  }*/
+  }
+});
