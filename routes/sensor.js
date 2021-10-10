fdc672ba7176cd2083182ff3a90929a077599073
Leo Bowen Wang <59091252+leobowenwang@users.noreply.github.com>
2021-10-10 22:51:13 +0200

    post to put

--- routes/sensor.js
+++ routes/sensor.js
@@ -43,7 +43,7 @@ router.post('/', async (req, res) => {
     TableName: dynamodbTableName,
     Item: req.body
   }
-  await dynamodb.post(params).promise().then(() => {
+  await dynamodb.put(params).promise().then(() => {
     const body = {
       Operation: 'SAVE',
       Message: 'SUCCESS',
