9206019e2b4c7ac6e8971ad64b6e017dcb79b6f1
Leo Bowen Wang <59091252+leobowenwang@users.noreply.github.com>
2021-10-10 21:01:45 +0200

    major update, added dynamo db

---
+++ routes/sensor.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const AWS = require('aws-sdk');
+AWS.config.update({
+  region: 'us-east-2'
+});
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamodbTableName = 'medpass-api';
+
+router.get('/', async (req, res) => {
+  const params = {
+    TableName: dynamodbTableName,
+    Key: {
+      'sensorId': req.query.sensorId
+    }
+  }
+  await dynamodb.get(params).promise().then(response => {
+    res.json(response.Item);
+  }, error => {
+    console.error('Do your custom error handling here. I am just ganna log it out: ', error);
+    res.status(500).send(error);
+  })
+})
+
+router.get('/all', async (req, res) => {
+  const params = {
+    TableName: dynamodbTableName
+  }
+  try {
+    const allData = await scanDynamoRecords(params, []);
+    const body = {
+      products: allData
+    }
+    res.json(body);
+  } catch(error) {
+    console.error('Oh no.', error);
+    res.status(500).send(error);
+  }
+})
+
+router.post('/', async (req, res) => {
+  const params = {
+    TableName: dynamodbTableName,
+    Item: req.body
+  }
+  await dynamodb.put(params).promise().then(() => {
+    const body = {
+      Operation: 'SAVE',
+      Message: 'SUCCESS',
+      Item: req.body
+    }
+    res.json(body);
+  }, error => {
+    console.error('Oh no.', error);
+    res.status(500).send(error);
+  })
+})
+
+async function scanDynamoRecords(scanParams, itemArray) {
+  try {
+    const dynamoData = await dynamodb.scan(scanParams).promise();
+    itemArray = itemArray.concat(dynamoData.Items);
+    if (dynamoData.LastEvaluatedKey) {
+      scanParams.ExclusiveStartKey = dynamoData.LastEvaluatedKey;
+      return await scanDynamoRecords(scanParams, itemArray);
+    }
+    return itemArray;
+  } catch(error) {
+    throw new Error(error);
+  }
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,39 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
+
+const sensorRouter = require('./routes/sensor');
 
 app.use(express.json());
+app.use('/sensor', sensorRouter);
 
-app.listen(process.env.PORT, 5000, () => console.log("API running on " + process.env.PORT + ":" + 5000));
+app.get('/', (req, res) => {
+  res.send('<h1>MedPass API</h1> <h4>Message: Success</h4><p>Version: 1.0</p>');
+})
 
-app.get("/api/sensor", (req, res) => {
-  res.status(200).send({
-    msg: 12345
-  });
-});
+app.get('/test', (req, res) => {
+  res.send();
+})
 
-app.post("/api/sensor/data", (req, res) => {
-  const { key, temp, hum, status } = req.body;
-
-  if (!key) {
-    res.status(418).send({ msg: "418 I'm a teapot" });
-  }
-
-  if (status == 404) {
-    res.status(404).send({ msg: "404 Failed to read sensor" });
-  }
-  if (status == 400) {
-    res.status(400).send({ msg: "400 Invalid sensor readings" });
-  }
-  if ((key, temp, hum)) {
-    res.status(200).send({
-      key: key,
-      temp: temp,
-      hum: hum,
-    });
-    /*try {
-    contract(key, temp);
-  } catch(error) {
-    console.log(error);
-  }*/
-  }
-});
+app.listen(port, () => {
+  console.log('Demo app is up and listening to port: ' + port);
+})
